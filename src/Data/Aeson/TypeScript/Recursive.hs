4c2f7a8e5ff15cb7d0c135f8ab3e272a93504e4d
ggzor <30713864+ggzor@users.noreply.github.com>
2022-08-25 19:52:35 +0000

    Use list instead of Set for `getTransitiveClosure`

--- src/Data/Aeson/TypeScript/Recursive.hs
+++ src/Data/Aeson/TypeScript/Recursive.hs
@@ -7,13 +7,13 @@ module Data.Aeson.TypeScript.Recursive (
 import Data.Aeson.TypeScript.Instances ()
 import Data.Aeson.TypeScript.TH
 import Data.Function
-import qualified Data.Set as S
+import Data.Containers.ListUtils (nubOrd)
 
 
-getTransitiveClosure :: S.Set TSType -> S.Set TSType
-getTransitiveClosure initialTypes = fix (\loop items -> let items' = S.unions (items : [getMore x | x <- S.toList items]) in
+getTransitiveClosure :: [TSType] -> [TSType]
+getTransitiveClosure initialTypes = fix (\loop items -> let items' = nubOrd (items ++ concatMap getMore items) in
                                             if | items' == items -> items
                                                | otherwise -> loop items'
                                         ) initialTypes
-  where getMore :: TSType -> S.Set TSType
-        getMore (TSType x) = S.fromList $ getParentTypes x
+  where getMore :: TSType -> [TSType]
+        getMore (TSType x) = getParentTypes x
